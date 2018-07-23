993b46b91ecf85b1fb41ab0c8ea0b36c9810c182
DonitaaJulius <donitaajulius@gmail.com>
2018-07-23 23:20:19 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,9 +1,13 @@
 def reins 
-  
+  puts "Holding your arms out in front of you, straight and at         chest level"
+puts "Cross your right wrist over your left and hold them together"
+puts "Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times."
+end 
 
 def lasso
   put "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
 end 
+
 
 lasso
 lasso
